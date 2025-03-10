5a1fbe8d7b0265cd18330a4105ba99e95aec7fe2
Talipov S.N. <proffix4@gmail.com>
2025-03-10 20:28:48 +0500

    rev. 20250310202848

--- MinGWUnicodeRelease/templates/dialogblocks_visual/CLEANING.bat
+++ MinGWUnicodeRelease/templates/dialogblocks_visual/CLEANING.bat
@@ -11,4 +11,10 @@ for %%f in (linux_app) do del /Q /F "%%f" >nul 2>&1
 for %%f in (*-autosave.pjd) do del /Q /F "%%f" >nul 2>&1
 for %%f in (*.rc) do del /Q /F "%%f" >nul 2>&1
 
+for %%d in (Debug MinGWUnicodeDebug MinGWUnicodeRelease Release) do (
+    if exist "%%d" (
+        rmdir /S /Q "%%d"
+    )
+)
+
 echo Cleaning of project configurations is completed.
